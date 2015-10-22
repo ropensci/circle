9a8b85c8dd73d2c949c6e6c981f7b51dc1cf5ccc
Thomas J. Leeper <thosjleeper@gmail.com>
2015-10-22 09:59:07 +0100

    outline test suite (#1)

---
+++ tests/testthat/tests.R
@@ -0,0 +1,33 @@
+context("authentication")
+
+test_that("login successful", {
+  TRUE
+})
+
+context("get projects")
+
+test_that("login successful", {
+  TRUE
+})
+
+
+context("builds")
+
+test_that("get builds", {
+  TRUE
+})
+
+test_that("cancel and retry builds", {
+  TRUE
+})
+
+
+context("environment variables")
+
+test_that("add environment variables", {
+  TRUE
+})
+
+test_that("delete environment variables", {
+  TRUE
+})
