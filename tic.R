0f007ddad7187f4605560ae3b47ef86ed310aece
pat-s <patrick.schratz@gmail.com>
2019-12-31 10:02:22 +0100

    install rotemplate in before_deply()

--- tic.R
+++ tic.R
@@ -1,8 +1,7 @@
-get_stage("install") %>%
-  add_step(step_install_github("ropensci/rotemplate"))
-
 do_package_checks()
 
 if (ci_on_circle()) {
+  get_stage("before_deploy") %>%
+    add_step(step_install_github("ropensci/rotemplate"))
   do_pkgdown(orphan = TRUE)
 }
