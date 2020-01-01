9f5e9631a75111252f91703c576418518951a939
pat-s <patrick.schratz@gmail.com>
2020-01-01 21:22:47 +0100

    fix tests

--- tests/testthat/test-print.R
+++ tests/testthat/test-print.R
@@ -1,7 +1,7 @@
 context("print S3 methods")
 
 test_that("s3 print method for 'circle_pipeline' works", {
-  out <- get_pipelines()
+  out <- get_pipelines(repo = repo, user = user)
   capture.output(expect_message(
     print(out),
     "A collection of 20 Circle CI pipelines"
@@ -9,7 +9,7 @@ test_that("s3 print method for 'circle_pipeline' works", {
 })
 
 test_that("s3 print method for 'circle_workflow' works", {
-  out <- get_workflows()
+  out <- get_workflows(repo = repo, user = user)
   capture.output(expect_message(
     print(out),
     "A collection of 10 Circle CI workflows"
@@ -17,7 +17,7 @@ test_that("s3 print method for 'circle_workflow' works", {
 })
 
 test_that("s3 print method for 'circle_job' works", {
-  out <- get_jobs()
+  out <- get_jobs(repo = repo, user = user)
   capture.output(expect_message(
     print(out),
     "A collection of 15 Circle CI jobs"
