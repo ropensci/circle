933e9add13834f90ca1eb16bcdaf818c071bf163
pat-s <patrick.schratz@gmail.com>
2020-09-23 22:16:22 +0200

    try new filtering approach

--- tests/testthat/helper-circle.R
+++ tests/testthat/helper-circle.R
@@ -1,6 +1,11 @@
 library("vcr")
 invisible(vcr::vcr_configure(
-  filter_sensitive_data = list("R_CIRCLE" = Sys.getenv("R_CIRCLE")),
-  dir = "../fixtures"
+  filter_sensitive_data = list(
+    "<<<R_CIRCLE>>>" = Sys.getenv("R_CIRCLE"),
+    "<<<PAT_GITHUB>>>" = Sys.getenv("PAT_GITHUB")
+  ),
+  dir = "../fixtures",
+  log = FALSE,
+  log_opts = list(file = "console")
 ))
 vcr::check_cassette_names()
