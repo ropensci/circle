938100642973496fff3836206e4b55aefe491f68
pat-s <patrick.schratz@gmail.com>
2020-11-03 11:03:46 +0100

    get GH Token on CI conditionally

--- tests/testthat/test-helpers-github.R
+++ tests/testthat/test-helpers-github.R
@@ -7,7 +7,11 @@ test_that("github helper functions work", {
   withr::with_dir(paste0(tempdir(), "/circle"), {
 
     # github_info() ------------------------------------------------------------
-    info <- github_info()
+    if (Sys.getenv("CI") != "") {
+      info <- github_info(.token = Sys.getenv("PAT_GITHUB"))
+    } else {
+      info <- github_info()
+    }
     expect_s3_class(info, "gh_response")
     expect_equal(info$name, "circle")
     expect_equal(info$owner$login, "ropenscilabs")
