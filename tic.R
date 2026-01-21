8da41a870ad9e00f0c65ca2ca79f103cb5dbe7c1
munoztd0 <david.munoztord@mailbox.org>
2026-01-21 15:50:02 +0100

    fix: windows warnings

--- tic.R
+++ tic.R
@@ -1,5 +1,11 @@
 do_package_checks(codecov = FALSE)
 
+get_stage("script") %>%
+  add_step(step_rcmdcheck(
+    args = c("--as-cran", "--no-manual"),
+    error_on = "warning"
+  ))
+
 if (ci_on_circle()) {
   get_stage("before_deploy") %>%
     add_step(step_install_github("ropensci/rotemplate"))
